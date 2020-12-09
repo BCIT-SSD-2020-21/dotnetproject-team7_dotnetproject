c60f8da539a1fcb306b08135cea10eb3cb19c008
crystkim3 <skim552@my.bcit.ca>
2020-12-08 17:19:52 -0800

    #16 delete unused import

--- korflix/src/components/Banner.js
+++ korflix/src/components/Banner.js
@@ -1,41 +1,41 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "../axios";
 import requests from "../requests";
 
 export default function Banner() {
     const [movie, setMovie] = useState([]);
 
-    useEffect(()=>{
-        async function fetchData(){
+    useEffect(() => {
+        async function fetchData() {
             const request = await axios.get(requests.koreanDrama);
             console.log(request.data.results)
             setMovie(
-            request.data.results[
+                request.data.results[
                 Math.floor(Math.random() * request.data.results.length)
-            ]
-        );
-        return request;
+                ]
+            );
+            return request;
         }
         fetchData()
-    },[])
+    }, [])
 
-    
+
 
     console.log(movie);
 
     return (
-        <header className="banner" 
+        <header className="banner"
             style={{
                 backgroundSize: "cover",
                 backgroundImage: `url(http://image.tmdb.org/t/p/original/${movie?.backdrop_path || movie?.poster_path})`,
                 backgroundPosition: "center center",
             }}>
-                <div className="banner__contents">
-                    <h1 className="banner__title">
-                        {movie?.title || movie?.name || movie?.original_name}
-                    </h1>
-        <h2 className="banner__desc">{movie.overview}</h2>
-                </div>
+            <div className="banner__contents">
+                <h1 className="banner__title">
+                    {movie?.title || movie?.name || movie?.original_name}
+                </h1>
+                <h2 className="banner__desc">{movie.overview}</h2>
+            </div>
         </header>
     )
 }
