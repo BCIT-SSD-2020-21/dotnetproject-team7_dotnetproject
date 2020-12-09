41ebaf3510d2ee3355c9882d7b059f17dc34fbe2
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2020-12-08 16:05:57 -0800

    fetch data from the API and render on the browser #18

--- korflix/src/components/Header.js
+++ korflix/src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 import NavBar from "./NavBar";
-
+import Banner from "./Banner"
+;
 export default function Header() {
     return (
         <div>
             <NavBar />
-            
+            <Banner />
         </div>
     )
 }
