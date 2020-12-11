2b1f966e3ee0dabf02f10573d566df15e352d087
crystkim3 <skim552@my.bcit.ca>
2020-12-11 13:09:16 -0800

    #29 #33 movies are displayed when user search the movie

--- korflix/src/components/Row.js
+++ korflix/src/components/Row.js
@@ -6,31 +6,34 @@ import SearchForm from './SearchForm';
 
 const Row = () => {
     const [movies, setMovies] = useState([]);
-    const [searchResults, setSearchResults] = useState('');
+    const [searchString, setSearchString] = useState('');
 
-    const getMovieRequest = async () => {
-        const url = "https://api.themoviedb.org/3/search/movie?api_key=59ff1e5e8dcca98569ad357b9d91a30b&query=frozen"
+    const getMovieRequest = async (searchString) => {
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=59ff1e5e8dcca98569ad357b9d91a30b&query=${searchString}`
         const response = await fetch(url);
         const responseJson = await response.json();
 
-        console.log(responseJson.results); //------ working 
+        //console.log(responseJson.results); //------ working 
 
 
-        setMovies(responseJson.results);
+        if (responseJson.results) {
+            setMovies(responseJson.results);
+        }
 
         return responseJson;
     };
 
     useEffect(() => {
-        getMovieRequest();
-    }, []);
-    console.log(movies);
+        getMovieRequest(searchString);
+    }, [searchString]);
+
+    //console.log(movies);
     return (
 
         <div className="container-fluid movie-pic">
             <div className='row'>
                 <MovieListHeading heading="Movies" />
-                <SearchForm searchResults={searchResults} setSearchResults={setSearchResults} />
+                <SearchForm searchString={searchString} setSearchString={setSearchString} />
             </div>
             <div className='row'>
                 <MovieList movies={movies} />
