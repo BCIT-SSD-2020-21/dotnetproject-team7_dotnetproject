1d499731170dcc7fc33885688ec49273a9389ebe
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2020-12-10 23:43:13 -0800

    add login component and login form

--- korflix/src/components/Login.js
+++ korflix/src/components/Login.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import {Link} from "react-router-dom";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons'
 
 export default function Login() {
     return (
-        <div>
-            <h1>Hello from login</h1>
+        <div className="login-wrapper">
+          <h3 className="login__title">
+            KORFLIX
+          </h3>
+          <div className="login__form">
+            <div className="login__input-field">
+              <input type="text" placeholder="Email" className="login__input"/>
+            </div>
+            <div className="login__input-field">
+              <input type="password" placeholder="Password" className="login__input"/>
+              <FontAwesomeIcon clasName="login__input-icon" icon={faEye}/>
+            </div>
+          </div>
+          <div className="login__input-field">
+          <Link to="/recover">
+            <span>Forget password?</span>
+          </Link>
+          </div>
+          <div className="login__btn">
+            <button>Login</button>
+          </div>
+          <div className="login__input-field">
+            <span>Don't have an account? </span>
+            <Link>
+             Sign up here
+            </Link>
+          </div>
         </div>
     )
 }
