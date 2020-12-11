ed0c0e400cf63e2ad4fa913f2b1f6b8a64d28260
crystkim3 <skim552@my.bcit.ca>
2020-12-10 21:09:57 -0800

    #29 #33 add Heading

--- korflix/src/components/Row.js
+++ korflix/src/components/Row.js
@@ -4,6 +4,7 @@ import MovieList from './MovieList';
 
 const Row = () => {
     const [movies, setMovies] = useState([]);
+    const [search, setSearch] = useState('');
 
     const getMovieRequest = async () => {
         const url = "https://api.themoviedb.org/3/search/movie?api_key=59ff1e5e8dcca98569ad357b9d91a30b&query=avengers"
@@ -25,6 +26,9 @@ const Row = () => {
     return (
 
         <div className="container-fluid movie-pic">
+            <div className='row'>
+
+            </div>
             <div className='row'>
                 <MovieList movies={movies} />
             </div>
