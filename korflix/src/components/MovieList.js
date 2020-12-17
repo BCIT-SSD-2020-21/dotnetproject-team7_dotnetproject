7d2639a90192d552c22a07a5b555da9a916a71bf
crystkim3 <skim552@my.bcit.ca>
2020-12-16 20:16:27 -0800

    #57 remove see detail button and implemnet Link on movie poster

--- korflix/src/components/MovieList.js
+++ korflix/src/components/MovieList.js
@@ -11,15 +11,17 @@ const MovieList = (props) => {
 
         <>
             {props.movies.map(movie => (<div key={movie.movieId} className="image-container d-flex justify-content-start m-3">
-                <img src={`${base_url}${movie?.poster_path || movie?.backdrop_path}`} alt="movie" style={{ width: "200px", height: "280px" }}
-                />
-                <div className="overlay d-flex align-items-center justify-content-center">
-                    <h6>{movie.name}</h6>
-                </div>
-
                 <Link key={movie.movieId} to={`/movies/${movie.movieId}`}>
-                    <button type="button" className="btn btn-dark py-2 btn-block">See Details</button>
+                    <img src={`${base_url}${movie?.poster_path || movie?.backdrop_path}`} alt="movie" style={{ width: "200px", height: "280px" }}
+                    />
+                    <div className="overlay d-flex align-items-center justify-content-center">
+                        <h6>{movie.name}</h6>
+                    </div>
                 </Link>
+
+                {/* <Link key={movie.movieId} to={`/movies/${movie.movieId}`}>
+                    <button type="button" className="btn btn-dark py-2 btn-block">See Details</button> */}
+
             </div>)
             )}
         </>
