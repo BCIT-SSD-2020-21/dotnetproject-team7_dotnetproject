a9cbaa1e00d92f92018bdf314c3183d5c5aaaef3
crystkim3 <skim552@my.bcit.ca>
2020-12-17 17:03:20 -0800

    combining with main data

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -9,11 +9,11 @@ const base_url = "http://image.tmdb.org/t/p/original/";
 
 
 
-const MovieDetail = ({match}) => {
-        const history = useHistory();
+const MovieDetail = ({ match }) => {
+    const history = useHistory();
 
-        const id = match.params.id;
-        const [movie, setMovie] = useState([]);
+    const id = match.params.id;
+    const [movie, setMovie] = useState([]);
 
 
     useEffect(() => {
@@ -25,58 +25,57 @@ const MovieDetail = ({match}) => {
             return request;
         }
         fetchData()
-        },[])
+    }, [])
 
-        const postRating = (rating) =>{
-            fetch('https://korflixapi.azurewebsites.net/rating', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    movieId: id,
-                    userId: sessionStorage.getItem("userId"),
-                    rating: rating,
-                })
-              })
+    const postRating = (rating) => {
+        fetch('https://korflixapi.azurewebsites.net/rating', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                movieId: id,
+                userId: sessionStorage.getItem("userId"),
+                rating: rating,
+            })
+        })
 
+    }
+
+    const submitRating = (rating) => {
+        var isAuth = !!sessionStorage.getItem("bearer-token")
+        // console.log(token)
+        if (!isAuth) {
+            history.push("/login")
+        } else {
+            postRating(rating);
         }
+    }
 
-        const submitRating = (rating) => {
-            var isAuth = !!sessionStorage.getItem("bearer-token")
-            // console.log(token)
-            if (!isAuth){
-               history.push("/login")
-            }else{
-                postRating(rating);
-            }
-        }
-        
-        return (
-            <>
-                <div className="container d-flex">
-                    <div className="row">
-                        <div className="col detail-box mt-3">
+    return (
+        <>
+            <div className="container d-flex">
+                <div className="row">
+                    <div className="col detail-box mt-3">
                         <img src={`${base_url}${movie?.poster_path}`} alt={movie.name} />
 
-                            <div className="col col-md-7 title">
-                                <h3 className="detail-title">{movie.name}</h3>
-                                <div>
-                                    <p>Release Date: {movie.first_air_date} | Average Rating: {movie?.tmdB_average || movie?.korlfix_average}/5</p>
-                                    <hr />
-                                    <p>{movie.overview}</p>
-                                </div>
-                                <ReactStars 
+                        <div className="col col-md-7 title">
+                            <h3 className="detail-title">{movie.name}</h3>
+                            <div>
+                                <p>Release Date: {movie.first_air_date} | Average Rating: {movie?.tmdB_average || movie?.korlfix_average}/5</p>
+                                <hr />
+                                <p>{movie.overview}</p>
+                            </div>
+                            <ReactStars
                                 count={5}
                                 onChange={submitRating}
                                 size={24}
                                 color2={'#ffd700'} />
-                            </div>
                         </div>
-                        <FiHeart className="detail-icon" size={40} />
-
                     </div>
+                    <FiHeart className="detail-icon" size={40} />
+
                 </div>
             </div>
             <Comments />
